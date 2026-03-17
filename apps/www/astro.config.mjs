5c79727a28be9aa705379b916fbd0c145f0e678f
pehtoorismies <pehtorismies@gmail.com>
2026-03-17 12:38:06 +0200

    feat: add OG preview

--- apps/www/astro.config.mjs
+++ apps/www/astro.config.mjs
@@ -2,6 +2,7 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: process.env.SITE_URL || 'http://localhost:4321',
   vite: {
     plugins: [tailwindcss()],
   },
